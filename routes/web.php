883236fdfac4b4a249bbe9f6284846ef6aacdad8
廖文忠 <wcliao1962@gmail.com>
2017-11-06 18:08:05 +0800

    修正/home路由綁HomeController@index

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,14 @@ Route::auth();
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home.home');
-});
+
+Route::get('/home', 'HomeController@index');
+
 
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
 
 Route::get('/tracy',function (){throw new \Exception('Tracy works!');});
+
+
